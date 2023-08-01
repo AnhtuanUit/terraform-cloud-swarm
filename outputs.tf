c6c1c5fbd97b8e804ecc8d04db1cde20a7a5ad2b
AnhtuanUit <anhtuan7194@gmail.com>
2023-08-01 11:08:26 +0700

    Update output for quick test

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
-# output "ssh_init_token" {
-#   value = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.manager1.main_ip} docker swarm init --force-new-cluster \nTOKEN=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.manager1.main_ip} docker swarm join-token manager -q)\nssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.worker1.main_ip} docker swarm join --token $TOKEN ${vultr_instance.manager1.main_ip}:2377\nssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.worker2.main_ip} docker swarm join --token $TOKEN ${vultr_instance.manager1.main_ip}:2377"
-# }
+output "manager1" {
+  value = "API: http://${vultr_instance.manager1.main_ip}:3000\nMongoDB: http://${vultr_instance.manager1.main_ip}:27017"
+}
+output "worker1" {
+  value = "API: http://${vultr_instance.worker1.main_ip}:3000\nMongoDB: http://${vultr_instance.worker1.main_ip}:27017"
+}
 
-# output "main_ips" {
-#   value = "manager1: ${vultr_instance.manager1.main_ip}\n worker1: ${vultr_instance.worker1.main_ip}\n worker2: ${vultr_instance.worker2.main_ip}"
-# }
+output "worker2" {
+  value = "API: http://${vultr_instance.worker2.main_ip}:3000\nMongoDB: http://${vultr_instance.worker2.main_ip}:27017"
+}
+
+output "ssh" {
+  value = "manager1: ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.manager1.main_ip}\nworker1: ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.worker1.main_ip}\nworker2: ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_instance.worker2.main_ip}"
+}
